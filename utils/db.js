ee8f68a4d88e8b2f50d064d16d25536354ef02c8
medazza <midazza27@gmail.com>
2024-07-02 08:04:02 -0700

    task 1

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
-const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || "files_manager";
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 /**
@@ -14,8 +14,8 @@ class DBClient {
       if (!err) {
         // console.log('Connected successfully to server');
         this.db = client.db(DB_DATABASE);
-        this.usersCollection = this.db.collection("users");
-        this.filesCollection = this.db.collection("files");
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
       } else {
         this.db = false;
       }
